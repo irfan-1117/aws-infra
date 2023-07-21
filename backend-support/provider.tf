e5c8e31428ce66670f210953bd2ff21e24760d53
irfan-1117 <52486859+irfan-1117@users.noreply.github.com>
2023-07-21 09:39:56 +0530

    Update provider.tf

--- backend-support/provider.tf
+++ backend-support/provider.tf
@@ -6,13 +6,3 @@ variable "region" {
 provider "aws" {
   region = var.region
 }
-
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.8.0"
-    }
-  }
-}
